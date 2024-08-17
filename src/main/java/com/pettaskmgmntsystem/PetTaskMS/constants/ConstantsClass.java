251521f33fbc8de208a25cb6c8b1146a6c048eba
mxnv <vadim.beller@mail.ru>
2024-08-17 19:49:52 +0300

    master

--- src/main/java/com/pettaskmgmntsystem/PetTaskMS/constants/ConstantsClass.java
+++ src/main/java/com/pettaskmgmntsystem/PetTaskMS/constants/ConstantsClass.java
@@ -6,6 +6,7 @@ public class ConstantsClass {
     public static final String USERROLE = "USER";
     public static final String SECRETKEY = "F510C4B0354B33E8ED73016958603787042439D41EC31038150B1FA6986B6AA532415E9E6DCF1A60A3354F14CAA6CB2EBF573130E104FF296BDF20ED65D4BC4B";
     public static final String NOTBLANK_EXCEPTION = "Недопустимая попытка передать пустым параметр: ";
+    public static final String LINE_FEED = "\n";
     public static final String HIDE = "####information is hidden";
     public static final String IS_DELETE = "Сущность была удалена";
     public static final Integer REGIME_RECORD = 1;
--- src/main/java/com/pettaskmgmntsystem/PetTaskMS/restentities/sheduler/ShedulerEngineService.java
+++ src/main/java/com/pettaskmgmntsystem/PetTaskMS/restentities/sheduler/ShedulerEngineService.java
@@ -1,34 +1,59 @@
 package com.pettaskmgmntsystem.PetTaskMS.restentities.sheduler;
 
+import com.pettaskmgmntsystem.PetTaskMS.constants.ConstantsClass;
 import com.pettaskmgmntsystem.PetTaskMS.restentities.CaseDto;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Service
+@Slf4j
 public class ShedulerEngineService {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Scheduled(fixedDelay = 5000)
-    public void restCreateCase(){
-        ResponseEntity<CaseDto> responseCaseDto = new ResponseEntity<>(null);
-        String createCaseController = "http://localhost:8080/api/v2/case/create";
-        while (responseCaseDto != null) {
-            responseCaseDto = restTemplate.postForEntity(createCaseController, )
+    @Scheduled(fixedDelay = 10000)
+    public void restCreateCase() {
+        RestTemplate restTemplate = new RestTemplate();
+        String createCaseController = "http://localhost:8081/api/v2/case/update-case";
+        CaseDto caseDto = new CaseDto(randomPrimaryKey(), cycleGenerateWord(), LocalDateTime.now());
+        CaseDto caseDtoResult =
+                restTemplate.patchForObject(createCaseController, caseDto, CaseDto.class);
+        if (caseDtoResult != null){
+            log.info(caseDtoResult.getId() + ConstantsClass.LINE_FEED
+                    + caseDtoResult.getName() + ConstantsClass.LINE_FEED + caseDtoResult.getDateOfCreate() + ConstantsClass.LINE_FEED);
+        } else {
+            log.info(null);
         }
     }
 
-    public void getCase(){
-        ResponseEntity<CaseDto> responseGetCaseDto = new ResponseEntity<>(null);;
-        String getCase = "http://localhost:8080/api/v2/gen-info/{id}";
+    private ResponseEntity<Long> getCase() {
+        RestTemplate restTemplate = new RestTemplate();
+        String getCase = "http://localhost:8081/api/v2/case/all-entity/counts";
         Random random = new Random();
-        responseGetCaseDto = restTemplate.getForEntity(getCase, CaseDto.class, random.nextInt());
+        ResponseEntity<Long> responseGetCaseDto = restTemplate.getForEntity(getCase, Long.class);
+        return responseGetCaseDto;
+    }
+
+    private String cycleGenerateWord(){
+        Random random = new Random();
+        int a = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        while (a < 27) {
+            stringBuilder.append((char) random.nextInt(96, 123));
+            a++;
+        }
+        return stringBuilder.toString();
+    }
+
+    private int randomPrimaryKey(){
+        Random random = new Random();
+        return random.nextInt(ConstantsClass.REGIME_OVERWRITING, getCase().getBody().intValue() + ConstantsClass.REGIME_RECORD);
     }
 
 }
